359029c14f12fc6d47ee5444220ffb1fc4429331
codetaha <taha.kachwala21@gmail.com>
2018-01-25 17:04:52 +0200

    Create App Layout

---
+++ react-redux/src/components/App.js
@@ -0,0 +1,22 @@
+/**
+ * Created by taha on 9/10/17.
+ */
+
+import React, {PropTypes} from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <p>Header here..</p>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;

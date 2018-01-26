7b78e2c5fe03cde7141ca60521a2787c4bd438db
codetaha <taha.kachwala21@gmail.com>
2018-01-26 10:45:04 +0200

    Create Courses Page

---
+++ react-redux/src/components/course/CoursesPage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by taha on 9/10/17.
+ */
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  };
+}
+
+export default CoursesPage;

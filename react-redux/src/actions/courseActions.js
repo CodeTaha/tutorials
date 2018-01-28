49c9018a27b574f9a56b9d8a0247b9f215a574bc
codetaha <taha.kachwala21@gmail.com>
2018-01-28 23:34:12 +0200

    Action Types Constants

--- react-redux/src/actions/courseActions.js
+++ react-redux/src/actions/courseActions.js
@@ -1,6 +1,7 @@
 /**
  * Created by taha on 9/10/17.
  */
+import * as types from './actionTypes';
 export function createCourse(course) {
-  return {type: 'CREATE_COURSE', course};
+  return {type: types.CREATE_COURSE, course};
 }

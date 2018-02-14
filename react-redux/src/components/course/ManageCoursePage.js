8e1135879449a7ecfaf440ba8924751f2457fa50
codetaha <taha.kachwala21@gmail.com>
2018-02-14 19:01:57 +0200

    Update State view componentWillReceiveProps

--- react-redux/src/components/course/ManageCoursePage.js
+++ react-redux/src/components/course/ManageCoursePage.js
@@ -21,6 +21,13 @@ class ManageCoursePage extends React.Component {
     _.bindAll(this, 'updateCourseState', 'saveCourse');
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id !== nextProps.course.id) {
+      // Necessary to populate form when existing
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
   updateCourseState(event) {
     const field = event.target.name;
     let course = this.state.course;
@@ -53,8 +60,19 @@ ManageCoursePage.propTypes = {
   actions: PropTypes.object.isRequired
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id === id);
+  if(course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; // from the path `/course/:id`
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId)
+  }
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
       value: author.id,

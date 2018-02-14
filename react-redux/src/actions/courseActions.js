cde77034e3a004551812fcf014cf554860367308
codetaha <taha.kachwala21@gmail.com>
2018-02-15 00:43:08 +0200

    Error Handling

--- react-redux/src/actions/courseActions.js
+++ react-redux/src/actions/courseActions.js
@@ -3,7 +3,7 @@
  */
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from "./ajaxStatusActions";
+import {ajaxCallError, beginAjaxCall} from "./ajaxStatusActions";
 
 export function loadCoursesSuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
@@ -34,6 +34,7 @@ export function saveCourse(course) {
     return courseApi.saveCourse(course).then(savedCourse => {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw(error);
     });
   };

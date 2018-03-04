c99799a1a4fec43a2da7c2984e8c3c14784ae284
codetaha <taha.kachwala21@gmail.com>
2018-03-04 22:52:03 +0200

    Setup Production Redux Store

--- react-redux/src/store/configureStore.js
+++ react-redux/src/store/configureStore.js
@@ -3,14 +3,17 @@
  */
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+const middleware = process.env.NODE_ENV !== 'production' ?
+  [thunk, require('redux-immutable-state-invariant')()] :
+  [thunk];
 
 export default function configureStore(initialState) {
   // Call this function to add values of server-side rendering
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
+    applyMiddleware(...middleware)
   );
 }
